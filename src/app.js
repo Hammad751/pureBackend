e027c4ffa675bb4ca7ef73e88f7a4e7ddae5927e
virtuByVirtual <hammadvtech@gamil.com>
2024-01-13 12:59:41 +0500

    create register API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -8,9 +8,14 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-app.use(express.static("public"))
 app.use(express.json({limit:'32kb'}))
 app.use(express.urlencoded({extended: true, limit: '32kb'}))
+app.use(express.static("public"))
 app.use(cookieParser());
+
+// import Routes
+import userRouter from './routes/user.routes.js';
+
+app.use('/server/users', userRouter);
 
 export { app };
