4202f3d2fa5388ed0d2948e016ab0064e4cbbc58
virtuByVirtual <hammadvtech@gamil.com>
2024-01-19 18:58:18 +0500

    add user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
      login,
      logout, 
      registerUser, 
-     refreshAccessToken 
+     refreshAccessToken,
+     updatePassword,
+     getcurrentUser,
+     updateAccountDetails,
+     updateAvatar,
+     updateCoverImage,
+     getUserChannelProfile,
+     getWatchHistory
     } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,13 @@ router.route("/login").post(login);
 // secured routes
 router.route("/logout").post(verifyJWT, logout);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, updatePassword);
+router.route("/current-user").get(verifyJWT, getcurrentUser);
+router.route("/update-details").patch(verifyJWT, updateAccountDetails);
+router.route("/avatar").path(verifyJWT, upload.single("avatar"), updateAvatar);
+router.route("/cover-image").path(verifyJWT, upload.single("coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/hitory").get(verifyJWT, getWatchHistory);
+
 
 export default router;
